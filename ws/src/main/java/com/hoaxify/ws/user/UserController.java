845e050dea1661bf077dfbc7bf485da171194aea
diclebahceli <diclebahceli24@gmail.com>
2023-11-10 21:43:45 +0300

    create a user service

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -6,14 +6,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hoaxify.ws.shared.GenericMessage;
+
 @RestController
 public class UserController {
 
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
      
     @PostMapping("/api/1.0/users")
-    void createUser(@RequestBody User user){
-        userRepository.save(user);
+    GenericMessage createUser(@RequestBody User user){
+        userService.save(user);
+        return new GenericMessage("User saved");
     }
 }
