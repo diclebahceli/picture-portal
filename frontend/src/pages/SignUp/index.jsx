ada60c909062b1194fa7be8118de112852eb3044
diclebahceli <diclebahceli24@gmail.com>
2023-11-13 20:49:58 +0300

    fix the ui for register page

--- frontend/src/pages/SignUp/index.jsx
+++ frontend/src/pages/SignUp/index.jsx
@@ -7,50 +7,77 @@ export function SignUp() {
   const [password, setPassword] = useState();
   const [passwordR, setPasswordR] = useState();
 
-  const onSubmit =  (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
-    axios.post('/api/1.0/users', {
-        username,
-        email,
-        password,
-    })
+    axios.post("/api/1.0/users", {
+      username,
+      email,
+      password,
+    });
   };
   return (
-    <form onSubmit={onSubmit}>
-      <h1>Sign up</h1>
-      <div>
-        <label htmlFor="Username">Username</label>
-        <input
-          id="Username"
-          onChange={(event) => setUsername(event.target.value)}
-        />
+    <div className="container">
+      <div className="col-lg-6 offset-lg-3 col-sm-8 offset-sm-2">
+        <form onSubmit={onSubmit} className="card">
+          <div className="text-center card-header">
+            <h1>Sign up</h1>
+          </div>
+          <div className="card-body">
+            <div className="mb-3">
+              <label htmlFor="Username" className="form-label">
+                Username
+              </label>
+              <input
+                className="form-control"
+                id="Username"
+                onChange={(event) => setUsername(event.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                E-mail
+              </label>
+              <input
+                className="form-control"
+                id="email"
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                className="form-control"
+                id="password"
+                type="password"
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="passwordr" className="form-label">
+                Password Repeat
+              </label>
+              <input
+                className="form-control"
+                id="passwordr"
+                type="password"
+                onChange={(event) => setPasswordR(event.target.value)}
+              />
+            </div>
+            <div className="text-center">
+              <button
+                className="btn btn-primary"
+                disabled={
+                  !username || !email || !password || password !== passwordR
+                }
+              >
+                Sign up
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-      <div>
-        <label htmlFor="email">E-mail</label>
-        <input id="email" onChange={(event) => setEmail(event.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="passwordr">Password Repeat</label>
-        <input
-          id="passwordr"
-          type="password"
-          onChange={(event) => setPasswordR(event.target.value)}
-        />
-      </div>
-      <button
-        disabled={!username || !email || !password || password !== passwordR}
-        
-      >
-        Sign up
-      </button>
-    </form>
+    </div>
   );
 }
--- ws/src/main/java/com/hoaxify/ws/user/UserService.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserService.java
@@ -1,6 +1,8 @@
 package com.hoaxify.ws.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,7 +10,12 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    
     public void save(User user) {
+        String password = passwordEncoder.encode(user.getPassword());
+    
+        user.setPassword(password);
         userRepository.save(user);
         
     }
